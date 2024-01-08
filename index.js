f3640d93a30fbee21bd98de73d08e3dc03daf9ce
Linda Giorgadze <linda.giorgadze17b@geolab.edu.ge>
2024-01-08 21:31:20 +0400

    Javascript entry

---
+++ index.js
@@ -0,0 +1,46 @@
+const newVariable = "Hello World !";
+const number = 25;
+
+var variable1 = "Variable 1";
+// console.log("სტრიქონული მონაცემი: ", variable1);
+
+variable1 = 25;
+// console.log("რიცხვითი მონაცემი: ", variable1);
+
+let variable2 = null;
+
+// let x = true;
+// let y = false;
+
+let comparison = number == variable1;
+
+// console.log(comparison);
+
+// const var1 = "Hello";
+// const var2 = "World";
+// const greeting1 = var1 + " " + var2 + "!";
+// const greeting2 = `${var1} ${var2}!`;
+// const greeting3 = var1.concat(" ", var2, "!");
+
+// console.log(greeting1, greeting2, greeting3);
+
+
+// const userName = 'Giorgi';
+// const userAge = 20;
+// const userInfo = userName.concat(': ', userAge);
+// console.log(userInfo);
+
+
+// ==  "20" == 20 >>> true
+// ===  "20" === 20 >>> false
+// >
+// >=
+// <
+// <=
+// !=  "20" != 20 >>>   false
+// !==   "20" !== 20 >>>   true
+
+let x = '40';
+let y = 40;
+let z = x == y;
+console.log(!z);
